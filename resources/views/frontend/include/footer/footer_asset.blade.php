b1464ed4c84a65944681c00d807f028602bbec6e
jsjisan <jisan@codeoptimizer.net>
2021-04-06 23:36:10 +0600

    shopping cart on process

--- resources/views/frontend/include/footer/footer_asset.blade.php
+++ resources/views/frontend/include/footer/footer_asset.blade.php
@@ -1,4 +1,6 @@
-<script type="text/javascript">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script type="text/javascript">
         (function () {
             var c = document.body.className;
             c = c.replace(/woocommerce-no-js/, 'woocommerce-js');
@@ -13,34 +15,34 @@
         /* <![CDATA[ */
         var wpcf7 = {"apiSettings":{"root":"https:\/\/milkybd.com\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"},"cached":"1"};
         /* ]]> */
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/contact-form-7/includes/js/scripts7752.js?ver=5.2.1' id='contact-form-7-js'>
-    </script>
+    </script> --}}{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/jquery-blockui/jquery.blockUI.min44fd.js?ver=2.70' id='jquery-blockui-js'>
-    </script>
+    </script> --}}
     <script type='text/javascript' id='wc-add-to-cart-js-extra'>
         /* <![CDATA[ */
         var wc_add_to_cart_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/?wc-ajax=%%endpoint%%","i18n_view_cart":"View cart","cart_url":"https:\/\/milkybd.com\/cart\/","is_cart":"","cart_redirect_after_add":"no"};
         /* ]]> */
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.min89df.js?ver=4.6.0' id='wc-add-to-cart-js'>
     </script>
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min6b25.js?ver=2.1.4' id='js-cookie-js'>
-    </script>
+    </script> --}}
     <script type='text/javascript' id='woocommerce-js-extra'>
         /* <![CDATA[ */
             var woocommerce_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/?wc-ajax=%%endpoint%%"};
         /* ]]> */
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/frontend/woocommerce.min89df.js?ver=4.6.0' id='woocommerce-js'>
-    </script>
+    </script> --}}
     <script type='text/javascript' id='wc-cart-fragments-js-extra'>
         /* <![CDATA[ */
         var wc_cart_fragments_params = {"ajax_url":"\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/?wc-ajax=%%endpoint%%","cart_hash_key":"wc_cart_hash_c41b44e38f4ccc9a22f7757fe3c0fb57","fragment_name":"wc_fragments_c41b44e38f4ccc9a22f7757fe3c0fb57","request_timeout":"5000"};
         /* ]]> */
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/frontend/cart-fragments.min89df.js?ver=4.6.0' id='wc-cart-fragments-js'>
-    </script>
+    </script> --}}
     <script type='text/javascript' id='wc-cart-fragments-js-after'>
         jQuery( 'body' ).bind( 'wc_fragments_refreshed', function() {
             var jetpackLazyImagesLoadEvent;
@@ -58,21 +60,21 @@
     
     </script>
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/themes/flatsome/inc/extensions/flatsome-live-search/flatsome-live-searchf416.js?ver=3.11.0' id='flatsome-live-search-js'>
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/super-socializer/js/front/social_login/generalaf1a.js?ver=7.12.48' id='the_champ_ss_general_scripts-js'>
     </script>
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/super-socializer/js/front/social_login/commonaf1a.js?ver=7.12.48' id='the_champ_sl_common-js'>
-    </script>
+    </script> --}}
     <script type='text/javascript' id='thickbox-js-extra'>
         /* <![CDATA[ */
         var thickboxL10n = {"next":"Next >","prev":"< Prev","image":"Image","of":"of","close":"Close","noiframes":"This feature requires inline frames. You have iframes disabled or your browser does not support them.","loadingAnimation":"https:\/\/milkybd.com\/wp-includes\/js\/thickbox\/loadingAnimation.gif"};
         /* ]]> */
     </script>
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-includes/js/thickbox/thickboxab87.js?ver=3.1-20121105' id='thickbox-js'>
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/super-socializer/js/front/facebook/sdkaf1a.js?ver=7.12.48' id='the_champ_fb_sdk-js'>
     </script>
-    <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/super-socializer/js/front/facebook/commentingaf1a.js?ver=7.12.48' id='the_champ_fb_commenting-js'>
+    <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/super-socializer/js/front/facebook/commentingaf1a.js?ver=7.12.48' id='the_champ_fb_commenting-js'> --}}
     </script>
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-includes/js/hoverIntent.minc245.js?ver=1.8.1' id='hoverIntent-js'>
     </script>
@@ -100,9 +102,9 @@
         /* <![CDATA[ */
         var wc_add_to_cart_variation_params = {"wc_ajax_url":"\/?wc-ajax=%%endpoint%%","i18n_no_matching_variations_text":"Sorry, no products matched your selection. Please choose a different combination.","i18n_make_a_selection_text":"Please select some product options before adding this product to your cart.","i18n_unavailable_text":"Sorry, this product is unavailable. Please choose a different combination."};
         /* ]]> */
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart-variation.min89df.js?ver=4.6.0' id='wc-add-to-cart-variation-js'>
-    </script>
+    </script> --}}
     <script type='text/javascript' id='zxcvbn-async-js-extra'>
         /* <![CDATA[ */
         var _zxcvbnSettings = {"src":"https:\/\/milkybd.com\/wp-includes\/js\/zxcvbn.min.js"};
@@ -134,6 +136,6 @@
         /* <![CDATA[ */
         var wc_password_strength_meter_params = {"min_password_strength":"3","stop_checkout":"","i18n_password_error":"Please enter a stronger password.","i18n_password_hint":"Hint: The password should be at least twelve characters long. To make it stronger, use upper and lower case letters, numbers, and symbols like ! \" ? $ % ^ & )."};
         /* ]]> */
-    </script>
+    </script>{{-- 
     <script type='text/javascript' src='{{ asset('/') }}/public/frontend/assets/wp-content/plugins/woocommerce/assets/js/frontend/password-strength-meter.min89df.js?ver=4.6.0' id='wc-password-strength-meter-js'>
-    </script>
+    </script> --}}
